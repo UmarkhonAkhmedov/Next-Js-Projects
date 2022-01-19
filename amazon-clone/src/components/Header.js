4478f6276c68a8764ab9ab28b05600384c90f0ec
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-19 21:27:30 +0500

    Adding and Styling ride side of top of header

--- amazon-clone/src/components/Header.js
+++ amazon-clone/src/components/Header.js
@@ -17,7 +17,22 @@ function Header() {
           <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4' type="text"/>
           <SearchIcon className="h-12 p-4"/>
         </div>
-        
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello Umarkhon</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+            <ShoppingCartIcon className="h-10"/>
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+          </div>
+        </div>
+
       </div>
 
 
